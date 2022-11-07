d946d82fb74abccc7df3d30630cb23f714fabf47
stevenalzate1 <117117323+stevenalzate1@users.noreply.github.com>
2022-11-07 12:47:47 -0500

    Update Conexion.java

--- GestionConjuntos/src/main/java/datos/Conexion.java
+++ GestionConjuntos/src/main/java/datos/Conexion.java
@@ -10,7 +10,7 @@ import org.apache.commons.dbcp2.BasicDataSource;
  */
 public class Conexion {
 
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/control_salud?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3308/gestion_conjunto?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASSWORD = "12345";
     private static BasicDataSource dataSource;
